2965ff85e5e5e68544a2fb9e9dcdd8111ce097e2
Arjun <loheraarjun@gmail.com>
2019-11-24 23:39:42 +0530

    bug fix

--- application/models/SiteModel.php
+++ application/models/SiteModel.php
@@ -6,14 +6,14 @@ class SiteModel extends CI_Model {
 
     public function get_books($limit = null){
         if($limit == null) {
-            return $this->db->where(array('is_approved' => 1, 'is_deleted' => 0, 'is_purchased' => 0))->order_by('uploaded_datetime', 'DESC')->get('Books')->result();
+            return $this->db->where(array('is_approved' => 1, 'is_deleted' => 0, 'is_purchased' => 0))->order_by('uploaded_datetime', 'DESC')->get('books')->result();
         } else {
-            return $this->db->where(array('is_approved' => 1, 'is_deleted' => 0, 'is_purchased' => 0))->order_by('uploaded_datetime', 'DESC')->limit($limit)->get('Books')->result();
+            return $this->db->where(array('is_approved' => 1, 'is_deleted' => 0, 'is_purchased' => 0))->order_by('uploaded_datetime', 'DESC')->limit($limit)->get('books')->result();
         }
     }
 
     public function get_newly_added_books(){
-        return $this->db->where(array('is_approved' => 1, 'is_deleted' => 0, 'is_purchased' => 0))->order_by('uploaded_datetime', 'DESC')->limit(4)->get('Books')->result();
+        return $this->db->where(array('is_approved' => 1, 'is_deleted' => 0, 'is_purchased' => 0))->order_by('uploaded_datetime', 'DESC')->limit(4)->get('books')->result();
     }
 
     public function get_pdfs() {
@@ -25,11 +25,11 @@ class SiteModel extends CI_Model {
     }
 
     public function get_book_info($book_id) {
-        return $this->db->where(array('book_id' => $book_id))->get('Books')->row();
+        return $this->db->where(array('book_id' => $book_id))->get('books')->row();
     }
 
     public function get_seller_info($book_id) {
-        $user_id = $this->db->where('book_id', $book_id)->get('Books')->row()->uploaded_by_user_id;
+        $user_id = $this->db->where('book_id', $book_id)->get('books')->row()->uploaded_by_user_id;
         $seller_info = $this->db->where('user_id', $user_id)->get('Users')->row();
         return $seller_info;
     }
